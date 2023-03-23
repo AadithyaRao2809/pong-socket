82400388a606c0d16f74aec04fa028473ff7d48c
AayushNagarr <92391624+AayushNagarr@users.noreply.github.com>
2023-03-23 23:34:47 +0530

    basic

--- Assets/Scripts/ballbounce.cs
+++ Assets/Scripts/ballbounce.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class ballbounce : MonoBehaviour
 {
     Rigidbody2D rb;
+        Vector3 LastVelocity;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        LastVelocity = rb.velocity;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var speed = LastVelocity.magnitude;
+        var direction = Vector3.Reflect(LastVelocity.normalized, collision.contacts[0].normal);
+        rb.velocity = direction * Mathf.Max(speed, 0f);
     }
 }
